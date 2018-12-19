92dbc09b2ba481482e78ae32e9ac5ec713307d46
marcelzoller <marcel@zoller.li>
2018-12-19 19:41:51 +0100

    fix fluxinstall

--- postroot.sh
+++ postroot.sh
@@ -60,5 +60,12 @@ echo "<INFO> Plugin Data folder is: $PDATA"
 echo "<INFO> Plugin Log folder (on RAMDISK!) is: $PLOG"
 echo "<INFO> Plugin CONFIG folder is: $PCONFIG"
 
+# Install Python flux_led Modul
+echo "<INFO> Install Python Module flux_led"
+python -m pip install -U pip setuptools
+cd /tmp/uploads/$PTEMPDIR/flux_led-0.22/
+python /tmp/uploads/$PTEMPDIR/flux_led-0.22/setup.py install 
+
+
 # Exit with Status 0
 exit 0
